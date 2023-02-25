ce223d8a2b002bb4b1ae17f411f77e4d9ec25920
bijoyvbabu123 <73835543+bijoyvbabu123@users.noreply.github.com>
2023-02-26 04:34:16 +0530

    created custom user model. Tested it with admin. working fine.

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+from .forms import UserCreationForm
 
 # Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    add_form = UserCreationForm
+    # model = User
+    list_display = ['email', 'first_name', 'last_name', 'is_staff', 'is_active', 'is_superuser', 'date_joined']
+    list_filter = ('is_staff', 'is_active', 'is_superuser')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'is_staff', 'is_active', 'is_superuser', 'password1', 'password2'),
+        }),
+    )
+    search_fields = ('email',)
+    ordering = ('email','date_joined')
+    filter_horizontal = ()
+
+
+admin.site.register(User, CustomUserAdmin)
---
+++ authentication/forms.py
@@ -0,0 +1,39 @@
+from django import forms
+from django.core.exceptions import ValidationError
+
+from .models import User
+
+class UserCreationForm(forms.ModelForm):
+    """
+    A form that creates a user, with no privileges, from the given email and
+    password.
+    """
+    error_messages = {
+        'password_mismatch': "The two password fields didn't match.",
+    }
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Password confirmation", widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        # fields = ("email",)
+        fields = ("email", "first_name", "last_name", "is_staff", "is_active") #fields to display in the form
+    
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise ValidationError(
+                self.error_messages['password_mismatch'],
+                code='password_mismatch',
+            )
+        return password2
+    
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
